c08c2540aa9d0c7dd10c847cc4bac78ddd704502
linhbkph29468 <linhbkph29468@gmail.com>
2023-07-30 09:42:41 +0700

    asm_add

--- asm/src/pages/admin/add.jsx
+++ asm/src/pages/admin/add.jsx
@@ -2,5 +2,24 @@ import React, { useState } from "react";
 
 const AddProductPage = ({ addProduct }) => {
     const [inputValues, setInputValues] = useState();
-    const onHandles
+    const onHandleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputValues({ ...inputValues, [name]: value });
+    }
+    const onHandleSubmit = (e) => {
+        e.preventDefault();
+        addProduct(inputValues);
+        // console.log(inputValues);
+    }
+    return (
+        <div>
+            <form action="" onSubmit={onHandleSubmit}>
+                <input type="text" placeholder='Enter Product Name' onChange={onHandleChange} name="name" />
+                <input type="text" placeholder='Enter Product Price' onChange={onHandleChange} name="price" />
+                <button type="submit">Add New</button>
+            </form>
+        </div>
+    )
 }
+export default AddProductPage
